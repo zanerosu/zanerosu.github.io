3db3515da1d25a314463ef6fe8b289ea96fd1dfd
zanerosu <zanerosu@gmail.com>
2024-01-05 19:20:23 -0500

    About Section Changes
    Added Fade-in to pictures

--- observer.js
+++ observer.js
@@ -1,25 +1,40 @@
-const faders = document.querySelectorAll
-('fade-in');
+const faders = document.querySelectorAll(".fade-in");
+const appearOptions = {
+    treshold: 1,
+    rootMargin:"0px 0px -200px 0px"
+};
 
-const appearOptions = {}
-
-const appearOnScroll = new IntersectionObserver
-(function(
-    entries, 
+const appearOnScroll = new IntersectionObserver(function(
+    entries,
     appearOnScroll
-    ){
-        entries.forEach(entry => {
-            if (!entry.isIntersecting){
-                return;
-            } else{
-                entry.target.classList.add('appear');
-                appearOnScroll.unobserve(entry.target);
-            }
-        })
-    }, 
-    appearOptions);
+  ) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("appear");
+        appearOnScroll.unobserve(entry.target);
+      }
+    });
+  },
+  appearOptions);
 
-    faders.forEach(fader => {
-        appearOnScroll.observe(fader);
-    })
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  });
 
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
